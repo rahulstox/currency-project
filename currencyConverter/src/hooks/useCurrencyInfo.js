1d439d5aa0b3d77cb12df8d4036053bfc823bec8
rahulstox <144617974+rahulstox@users.noreply.github.com>
2024-08-13 14:30:45 +0530

    commit

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,12 +1,14 @@
 import {useEffect, useState} from "react"
 
 // useEffect(()=>{callback},[dependencies array])
-
+// HOW TO DESIGN CUSTOM HOOKS
 function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json`)
+        // Here we convert the respone into the json the data of the api store in the form of a string that cnvert it into the json me
         .then((res) => res.json())
+        // NowThe response comes in the form of a json for holding a data  So jo res me jo data aayaha hai ushme se currrency nikal loj
         .then((res) => setData(res[currency]))
         console.log(data);
     }, [currency])
